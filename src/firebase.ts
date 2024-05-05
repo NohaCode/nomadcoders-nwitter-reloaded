83995b755e10a669a7b019f282420143cdbde4d5
NohaCode <gammagx@gmail.com>
2024-05-05 16:10:23 +0900

    #2.3 Firebase Project
    [노마드 코드 강의]
    강의 : 트위터 클론코딩
    목표
    - 파이어베이스 프로젝트 생성하기
    - react에서 npm i firebase 설치하고, 홈페이지에서 알려주는 sdk 코드 추가

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOody-oWg0S2cSSZD__ccL9ZAABG8IfaU",
+  authDomain: "nomadcoders-nwitter-reloaded.firebaseapp.com",
+  projectId: "nomadcoders-nwitter-reloaded",
+  storageBucket: "nomadcoders-nwitter-reloaded.appspot.com",
+  messagingSenderId: "255042596829",
+  appId: "1:255042596829:web:9397121073ed1c72509034",
+};
+
+const app = initializeApp(firebaseConfig);
